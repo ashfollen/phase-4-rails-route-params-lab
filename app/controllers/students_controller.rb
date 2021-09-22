bd9a9cf2f0af59d98ca63dc01a96cd6b77a3482b
Ashley Follen <ashleyfollen@outlook.com>
2021-09-22 15:18:31 -0600

    completed assignment

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,19 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    # bonus: use query params in the index action to return a list of all students whose first or last name matches the query parameter
+    students = if params[:name]
+                  Student.by_name(params[:name])
+                else
+                  Student.all
+                end 
     render json: students
+    
   end
+
+  def show
+    student = Student.find_by(id: params[:id])
+    render json: student
+  end 
 
 end
